a470d393ae2950158e4f0607b2044b4f8add3780
dieple <dieple1@gmail.com>
2020-02-09 19:49:06 +0000

    fixed quotes around dependon and type string

--- terraform-aws-key-pair/variables.tf
+++ terraform-aws-key-pair/variables.tf
@@ -1,5 +1,4 @@
 variable "key_name" {
-  type        = "string"
   description = "Application or solution name (e.g. `app`)"
 }
 
@@ -10,7 +9,6 @@ variable "tags" {
 }
 
 variable "ssh_public_key_path" {
-  type        = "string"
   description = "Path to SSH public key directory (e.g. `/secrets`)"
 }
 
@@ -20,25 +18,21 @@ variable "generate_ssh_key" {
 }
 
 variable "ssh_key_algorithm" {
-  type        = "string"
   default     = "RSA"
   description = "SSH key algorithm"
 }
 
 variable "private_key_extension" {
-  type        = "string"
   default     = ""
   description = "Private key extension"
 }
 
 variable "public_key_extension" {
-  type        = "string"
   default     = ".pub"
   description = "Public key extension"
 }
 
 variable "chmod_command" {
-  type        = "string"
   default     = "chmod 600 %v"
   description = "Template of the command executed on the private key file"
 }
